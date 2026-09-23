cb6e6e3c40d159a44eca852de6f05cc71d2829bf
BarbeRouss <julienrousselle@outlook.be>
2026-09-23 22:18:52 +0200

    reduce minimum password length to 8 characters across application and documentation

--- src/HouseFlow.Application/Generated/Contracts.g.cs
+++ src/HouseFlow.Application/Generated/Contracts.g.cs
@@ -95,12 +95,12 @@ namespace HouseFlow.Contracts
         public string Email { get; }
 
         /// <summary>
-        /// Minimum 12 caractères, avec au moins une majuscule, une minuscule, un chiffre et un caractère spécial
+        /// Minimum 8 caractères, avec au moins une majuscule, une minuscule, un chiffre et un caractère spécial
         /// </summary>
         [System.Text.Json.Serialization.JsonPropertyName("password")]
         [System.ComponentModel.DataAnnotations.Required]
-        [System.ComponentModel.DataAnnotations.StringLength(int.MaxValue, MinimumLength = 12)]
-        [System.ComponentModel.DataAnnotations.RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{12,}$")]
+        [System.ComponentModel.DataAnnotations.StringLength(int.MaxValue, MinimumLength = 8)]
+        [System.ComponentModel.DataAnnotations.RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$")]
         public string Password { get; }
 
         private System.Collections.Generic.IDictionary<string, object>? _additionalProperties;
